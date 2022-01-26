f80464b95b6ea94226ac14a0fc178392e9856cd6
Tino <tino.paajanen@hotmail.fi>
2022-01-26 20:25:07 +0200

    asema.cpp toteutus viikko2

--- shakki/asema.cpp
+++ shakki/asema.cpp
@@ -52,7 +52,7 @@ Asema::Asema()
 }
 
 
-void Asema::paivitaAsema(Siirto *siirto)
+void Asema::paivitaAsema(Siirto* siirto)
 {
 
 	// Kaksoisaskel-lippu on oletusarvoisesti pois p‰‰lt‰.
@@ -60,87 +60,113 @@ void Asema::paivitaAsema(Siirto *siirto)
 
 
 	//Tarkastetaan on siirto lyhyt linna
+	if (siirto->onkoLyhytLinna()) {
 
+	}
 
 	// onko pitk‰ linna
+	else if (siirto->onkoPitk‰linna()) {
 
+	}
 
 
 	// Kaikki muut siirrot
-
+	else {
 
 		//Ottaa siirron alkuruudussa olleen nappulan talteen 
-
+		Nappula* liikkuva = _lauta[siirto->getAlkuruutu().getSarake()][siirto->getAlkuruutu().getRivi()];
+		int nappulaKoodi = liikkuva->getKoodi();
 
 		//Laittaa talteen otetun nappulan uuteen ruutuun
-
+		_lauta[siirto->getLoppuruutu().getSarake()][siirto->getLoppuruutu().getRivi()];
 
 		// Tarkistetaan oliko sotilaan kaksoisaskel
 		// (asetetaan kaksoisaskel-lippu)
 
 		// Ohestalyˆnti on tyhj‰‰n ruutuun. Vieress‰ oleva (sotilas) poistetaan.
+		if (nappulaKoodi == VS || nappulaKoodi == MS) {
+			//// Katsotaan jos nappula on sotilas ja rivi on p‰‰tyrivi niin ei vaihdeta nappulaa 
+			////eli alkuruutuun laitetaan null ja loppuruudussa on jo kliittym‰n laittama nappula MIIKKA, ei taida minmaxin kanssa hehkua?
 
-		//// Katsotaan jos nappula on sotilas ja rivi on p‰‰tyrivi niin ei vaihdeta nappulaa 
-		////eli alkuruutuun laitetaan null ja loppuruudussa on jo kliittym‰n laittama nappula MIIKKA, ei taida minmaxin kanssa hehkua?
-
-		//
+		}
 		////muissa tapauksissa alkuruutuun null ja loppuruutuun sama alkuruudusta l‰htenyt nappula
-
+		else {
+			_lauta[siirto->getAlkuruutu().getSarake()][siirto->getAlkuruutu().getRivi()] = NULL;
+		}
 		// katsotaan jos liikkunut nappula on kuningas niin muutetaan onkoKuningasLiikkunut arvo (molemmille v‰reille)
+		if (nappulaKoodi == VK)
+			_onkoValkeaKuningasLiikkunut = true;
+		else if (nappulaKoodi == MK)
+			_onkoMustaKuningasLiikkunut = true;
 
 		// katsotaan jos liikkunut nappula on torni niin muutetaan onkoTorniLiikkunut arvo (molemmille v‰reille ja molemmille torneille)
-
+		else if (nappulaKoodi == VT) {
+			if (siirto->getAlkuruutu().getSarake() == 0) //daamin puoleinen valkoinen torni liikkunut
+				_onkoValkeaDTliikkunut = true;
+			else if (siirto->getAlkuruutu().getSarake() == 7) //kuninkaan puoleinen valkoinen torni liikkunut
+				_onkoValkeaKTliikkunut = true;
+		}
+		else if (nappulaKoodi == MT) {
+			if (siirto->getAlkuruutu().getSarake() == 0) //daamin puoleinen musta torni liikkunut
+				_onkoMustaDTliikkunut = true;
+			else if (siirto->getAlkuruutu().getSarake() == 7) //kuninkaan puoleinen musta torni liikkunut
+				_onkoMustaKTliikkunut = true;
+		}
+	}
 	//p‰ivitet‰‰n _siirtovuoro
-
+	if (_siirtovuoro == 0)
+		_siirtovuoro = 1;
+	else
+		_siirtovuoro = 0;
 }
 
 
 
-int Asema::getSiirtovuoro() 
+int Asema::getSiirtovuoro()
 {
-	return 0;
+	return _siirtovuoro;
 }
 
 
-void Asema::setSiirtovuoro(int vuoro) 
+void Asema::setSiirtovuoro(int vuoro)
 {
-	
+	_siirtovuoro = vuoro;
 }
 
 
-bool Asema::getOnkoValkeaKuningasLiikkunut() 
+bool Asema::getOnkoValkeaKuningasLiikkunut()
 {
-	return false;
+	return _onkoValkeaKTliikkunut;
 }
 
 
-bool Asema::getOnkoMustaKuningasLiikkunut() 
+bool Asema::getOnkoMustaKuningasLiikkunut()
 {
-	return false;
+	return _onkoMustaKuningasLiikkunut;
 }
 
 
-bool Asema::getOnkoValkeaDTliikkunut() 
+bool Asema::getOnkoValkeaDTliikkunut()
 {
-	return false;
+	return _onkoValkeaDTliikkunut;
 }
 
 
-bool Asema::getOnkoValkeaKTliikkunut() 
+bool Asema::getOnkoValkeaKTliikkunut()
 {
-	return false;
+	return _onkoValkeaKTliikkunut;
 }
 
 
-bool Asema::getOnkoMustaDTliikkunut() 
+bool Asema::getOnkoMustaDTliikkunut()
 {
-	return false;
+	return _onkoMustaDTliikkunut;
 }
 
 
-bool Asema::getOnkoMustaKTliikkunut() 
+bool Asema::getOnkoMustaKTliikkunut()
 {
-	return false;
+	return _onkoMustaKTliikkunut;
 }
 
 
@@ -158,44 +184,44 @@ vai olla est‰m‰ss‰ vastustajan korotusta siksi ei oteta kantaa
 3. Arvosta keskustaa sotilailla ja ratsuilla
 4. Arvosta pitki‰ linjoja daami, torni ja l‰hetti
 */
-double Asema::evaluoi() 
+double Asema::evaluoi()
 {
 	return 0;
 
 	//kertoimet asetettu sen takia ett‰ niiden avulla asioiden painoarvoa voidaan s‰‰t‰‰ helposti yhdest‰ paikasta
-	
+
 	//1. Nappuloiden arvo
-	
+
 	//2. Kuningas turvassa
-	
+
 	//3. Arvosta keskustaa
-	
+
 	// 4. Arvosta linjoja
-	
+
 }
 
 
-double Asema::laskeNappuloidenArvo(int vari) 
+double Asema::laskeNappuloidenArvo(int vari)
 {
 	return 0;
-	
+
 }
 
 
-bool Asema::onkoAvausTaiKeskipeli(int vari) 
+bool Asema::onkoAvausTaiKeskipeli(int vari)
 {
 	return 0;
 	// Jos upseereita 3 tai v‰hemm‰n on loppupeli
 	// mutta jos daami laudalla on loppueli vasta kun kuin vain daami j‰ljell‰
-	
+
 	//Jos vari on 0 eli valkoiset
 	//niin on keskipeli jos mustalla upseereita yli 2 tai jos daami+1
-	
+
 
 }
 
 
-double Asema::nappuloitaKeskella(int vari) 
+double Asema::nappuloitaKeskella(int vari)
 {
 	return 0;
 
@@ -203,30 +229,30 @@ double Asema::nappuloitaKeskella(int vari)
 	//ratsut ydinkeskustassa + 0.25/napa
 	//sotilaat laitakeskustassa + 0.11/napa
 	//ratsut laitakeskustassa + 0.11/napa
-	
+
 	//valkeille ydinkeskusta
 
-	
-	
+
+
 	//valkeille laitakeskusta
-	
+
 
 
 	//mustille ydinkeskusta
-	
+
 	//mustille laitakeskusta
-	
+
 }
 
 
-double Asema::linjat(int vari) 
+double Asema::linjat(int vari)
 {
 	return 0;
-	
+
 	//valkoiset
-	
+
 	//mustat
-	
+
 }
 
 
@@ -257,26 +283,26 @@ MinMaxPaluu Asema::minimax(int syvyys)
 	MinMaxPaluu paluuarvo;
 
 	// Generoidaan aseman lailliset siirrot.
-	
+
 	// Rekursion kantatapaus 1: peli on loppu
-	
+
 	// Rekursion kantatapaus 2: katkaisusyvyydess‰
-	
+
 	// Rekursioaskel: kokeillaan jokaista laillista siirtoa s
 	// (alustetaan paluuarvo huonoimmaksi mahdolliseksi).
-	
+
 	return paluuarvo;
 }
 
 
-MinMaxPaluu Asema::maxi(int syvyys) 
+MinMaxPaluu Asema::maxi(int syvyys)
 {
 	MinMaxPaluu paluu;
 	return paluu;
 }
 
 
-MinMaxPaluu Asema::mini(int syvyys) 
+MinMaxPaluu Asema::mini(int syvyys)
 {
 	MinMaxPaluu paluu;
 	return paluu;
@@ -290,12 +316,12 @@ bool Asema::onkoRuutuUhattu(Ruutu* ruutu, int vastustajanVari)
 }
 
 
-void Asema::huolehdiKuninkaanShakeista(std::list<Siirto>& lista, int vari) 
-{ 
-	
+void Asema::huolehdiKuninkaanShakeista(std::list<Siirto>& lista, int vari)
+{
+
 }
 
 
 void Asema::annaLaillisetSiirrot(std::list<Siirto>& lista) {
-	
+
 }
